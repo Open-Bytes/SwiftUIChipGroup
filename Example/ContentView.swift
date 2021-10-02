f7bdc6bb9801c1f4c65ff5da54ce2f6d11653de3
Shaban Kamel <sha@Shabans-MacBook-Pro.local>
2021-10-02 14:50:52 +0200

    Initial commit

---
+++ Example/ContentView.swift
@@ -0,0 +1,19 @@
+//
+// Created by Shaban on 25/05/2021.
+// Copyright (c) 2021 sha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
